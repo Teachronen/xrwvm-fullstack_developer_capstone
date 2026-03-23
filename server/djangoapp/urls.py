a4361ff0a96d8ce62cc426f12a6b2205fd833dba
Teachronen <rotem.ronen@gmail.com>
2026-03-23 08:07:52 -0400

    part 2 add a register functionality

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
@@ -11,6 +12,9 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
+    path('register/', views.registration, name='register'),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+
 
     # path for dealer reviews view
 
