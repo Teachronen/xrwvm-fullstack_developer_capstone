b82f2eb3c2318eebde526c674449d9f78177f404
Teachronen <rotem.ronen@gmail.com>
2026-03-25 08:28:34 -0400

    admin_logout

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -32,9 +32,12 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     "rotemronen-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "rotemronen-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
+
 CSRF_TRUSTED_ORIGINS = [
-     "https://rotemronen-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://rotemronen-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
+    "https://rotemronen-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
 ]
 
 REST_FRAMEWORK = {
